ea0eb6b03a2a83a5250ede71bed6c0f0c7b932b6
Per Steenstrup <per.steenstrup@gmail.com>
2018-04-04 13:19:53 -0700

    destroy & update added

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,5 +6,14 @@ module.exports = function(app){
 
     app.get('/newuser/:username', function(req,res){
         users.create(req, res)
-    })
+    });
+
+    app.get('/destroy/:username', function(req,res){
+        users.destroy(req,res)
+    });
+
+    app.get('/update/:oldUsername/:newUsername', function(req,res){
+        users.update(req,res)
+
+    });
 };
--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -11,5 +11,20 @@ module.exports = {
         User.create({name: req.params.username}, function(err, user){
             res.redirect('/');
         });
-    }
+    },
+
+    destroy: function(req,res){
+        User.remove({name: req.params.username}, function(err, user){
+            res.redirect('/')
+        })
+    },
+
+    update: function(req,res){
+        User.findOneAndUpdate({name:req.params.oldUsername},{$set: {name:req.params.newUsername}},function(err,outcome){
+            if(err){
+                console.log("something went wrong");
+            }
+            res.redirect("/");
+        })
+    },
 }
